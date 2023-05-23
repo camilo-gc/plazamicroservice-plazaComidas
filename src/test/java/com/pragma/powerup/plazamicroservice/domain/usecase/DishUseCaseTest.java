e8d7c93982c8f0d95e5eeb2444ab2d210935c655
camilo-gc <camilogarcia.dev@gmail.com>
2023-05-23 10:35:21 -0500

    feat: add unit test for updateDish

--- src/test/java/com/pragma/powerup/plazamicroservice/domain/usecase/DishUseCaseTest.java
+++ src/test/java/com/pragma/powerup/plazamicroservice/domain/usecase/DishUseCaseTest.java
@@ -1,6 +1,7 @@
 package com.pragma.powerup.plazamicroservice.domain.usecase;
 
 import com.pragma.powerup.plazamicroservice.adapters.driven.jpa.mysql.exceptions.CategoryNotFoundException;
+import com.pragma.powerup.plazamicroservice.adapters.driven.jpa.mysql.exceptions.DishNotFoundException;
 import com.pragma.powerup.plazamicroservice.adapters.driven.jpa.mysql.exceptions.RestaurantNotFoundException;
 import com.pragma.powerup.plazamicroservice.domain.api.IDishServicePort;
 import com.pragma.powerup.plazamicroservice.domain.model.Category;
@@ -54,12 +55,42 @@ class DishUseCaseTest {
     void saveDishSuccessful(){
 
         Category category = new Category( 1L, "", "" );
-        Restaurant restaurant = new Restaurant( 1L,"","",2L,"","","" );
+        Restaurant restaurant = new Restaurant( 1L,"","",null,"","","" );
         Dish dishReq = new Dish( null, "", category, "", 1L, restaurant, "", true );
         Dish dishRes = new Dish( 1L, "", category, "", 1L, restaurant, "", true );
 
         when(dishPersistencePort.saveDish(dishReq)).thenReturn(dishRes);
         assertNotNull(dishServicePort.saveDish(dishReq));
+
+    }
+
+
+    //TODO: updateDishTest
+    @Test
+    void updateDishNotFoundException(){
+
+        Dish dish = new Dish( 10L, "", null, "actualizando", 1000L, null, "", true );
+
+        doThrow(DishNotFoundException.class).when(dishPersistencePort).updateDish( dish );
+        assertThrows( DishNotFoundException.class, ()-> dishServicePort.updateDish(dish) );
+
+    }
+
+
+    @Test
+    void updateDishSuccessful(){
+
+        Category category = new Category( 1L, "Entrada", "plato suave para iniciar" );
+        Restaurant restaurant = new Restaurant( 1L, "pepe food", "string", 2L,
+                "+793247501667", "http://pepefood.com/recursos/logo.jpg", "111" );
+
+        Dish dishReq = new Dish( 1L, null, null, "actualizado", 12000L,
+                null, null, null );
+        Dish dishRes = new Dish( 1L, "pancitos", category, "actualizado", 12000L, restaurant,
+                "http://pepefood.com/platos/pancitos.jpg", true );
+
+        when(dishPersistencePort.updateDish(dishReq)).thenReturn(dishRes);
+        assertNotNull(dishServicePort.updateDish(dishReq));
 
     }
 
